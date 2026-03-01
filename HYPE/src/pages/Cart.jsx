85a65a01fdf4ea4068894dec2845276ef1ac3a03
Mahesh Kakunuri <maheshkakunuri9@gmail.com>
2026-03-01 23:30:27 +0530

    gitig

--- HYPE/src/pages/Cart.jsx
+++ HYPE/src/pages/Cart.jsx
@@ -381,7 +381,7 @@ const Cart = () => {
       });
 
       // const resp = await fetch("http://localhost:5000/api/payment/create-order", {
-      const resp = await fetch("https://hyperbitefullstack.onrender.com/api/payment/create-order", {
+      const resp = await fetch("https://hyperbitedeploy.onrender.com/api/payment/create-order", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -415,7 +415,7 @@ const Cart = () => {
         handler: async function (response) {
           try {
             // const verifyRes = await fetch("http://localhost:5000/api/payment/verify", {
-            const verifyRes = await fetch("https://hyperbitefullstack.onrender.com/api/payment/verify", {
+            const verifyRes = await fetch("https://hyperbitedeploy.onrender.com/api/payment/verify", {
               method: "POST",
               headers: { "Content-Type": "application/json" },
               body: JSON.stringify(response),
